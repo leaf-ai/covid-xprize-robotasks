243864e03467abd00f63d8aefca176b144f1482c
Olivier Francon <olivier.francon@cognizant.com>
2020-11-25 19:16:40 +0100

    Simplify Dockerfile and entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,6 @@
 # Start the run once job.
 echo "Docker container has been started"
 
-declare -p | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /home/xprize/container.env
-
-# Setup a cron schedule
-echo "SHELL=/bin/bash
-BASH_ENV=/home/xprize/container.env
-* * * * * /work/hello.sh >> /var/log/cron.log 2>&1
-# This extra line makes it a valid cron" > scheduler.txt
-
-crontab -u xprize scheduler.txt
-cron -f
+crontab - <<EOF
+* * * * * /home/xprize/work/hello.sh >> /tmp/cron.log 2>&1
+EOF
