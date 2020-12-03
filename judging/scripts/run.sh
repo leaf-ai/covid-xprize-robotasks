4f9c4a32d787f03bf3aafc2e99975ec2f2bf585d
darren.sargent <darren.sargent@cognizant.ai>
2020-12-03 09:26:19 -0800

    Create logs dir if necessary

--- judging/scripts/run.sh
+++ judging/scripts/run.sh
@@ -9,7 +9,14 @@ set -o nounset
 set -o pipefail
 set -o xtrace
 
+# Create logs dir if necessary
+logs_dir=$HOME/work/predictions/logs
+mkdir -p "$logs_dir"
+
+# Set up log file path
 log_file_name=predict_$(date +"%Y_%m_%d_%H%M").txt
-log_file_path=$HOME/work/predictions/logs/$log_file_name
-echo "Running predictions generator bootstrap..." &>> $log_file_path
-$HOME/work/bootstrap.sh &>> $log_file_path
+log_file_path=$logs_dir/$log_file_name
+
+# Launch bootstrap
+echo "Running predictions generator bootstrap..." &>> "$log_file_path"
+$HOME/work/bootstrap.sh &>> "$log_file_path"
